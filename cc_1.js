6cca97376d339624b2462803d5dd67d5c3b602cd
Markayla Montgomery <mm42@usf.edu>
2025-01-22 17:47:47 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -4,3 +4,9 @@ var isActive = true
 console.log("Employee Name:", employeeName, "Type:", typeof employeeName)
 console.log("Employee ID:", employeeID, "Type:", typeof employeeID)
 console.log("Is Active:", isActive, "Type:", typeof isActive)
+let productName = "Name of Product"
+const productPrice = 012
+var isAvailable = true
+console.log("Product Name:", productName, "Type:", typeof productName)
+console.log("Product Price:", productPrice, "Type:", typeof productPrice)
+console.log("Is Available:", isAvailable, "Type:", typeof isAvailable)
