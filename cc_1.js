532b04bef904a3e92c96d02d6de60599dc1f8aee
Markayla Montgomery <mm42@usf.edu>
2025-01-22 16:47:07 -0500

    Inititial commit - JavaScript Basics Challenge Setup

---
+++ cc_1.js
@@ -0,0 +1,6 @@
+let employeeName = "John Doe"
+const employeeID = 12345
+var isActive = true
+console.log("Employee Name:", employeeName, "Type:", typeof employeeName)
+console.log("Employee ID:", employeeID, "Type:", typeof employeeID)
+console.log("Is Active:", isActive, "Type:", typeof isActive)
